15c8e58a0eb6f7fb33d96e29267988da12909ee5
keystroke3 <keystroke33@gmail.com>
2021-05-28 21:30:51 +0300

    initial commit with everything working

---
+++ public/scripts/utils.js
@@ -0,0 +1,66 @@
+function copyLink() {
+	var text = document.getElementById("link-text").innerText;
+	var textArea = document.createElement("textarea");
+	textArea.style.position = "fixed";
+	textArea.style.top = 0;
+	textArea.style.left = 0;
+
+	// Ensure it has a small width and height. Setting to 1px / 1em
+	// doesn't work as this gives a negative w/h on some browsers.
+	textArea.style.width = "2em";
+	textArea.style.height = "2em";
+
+	// We don't need padding, reducing the size if it does flash render.
+	textArea.style.padding = 0;
+
+	// Clean up any borders.
+	textArea.style.border = "none";
+	textArea.style.outline = "none";
+	textArea.style.boxShadow = "none";
+	// Avoid flash of the white box if rendered for any reason.
+	textArea.style.background = "transparent";
+	textArea.value = text;
+
+	document.body.appendChild(textArea);
+	textArea.focus();
+	textArea.select();
+	try {
+		var successful = document.execCommand("copy");
+		var msg = successful ? "successful" : "unsuccessful";
+		console.log("Copying text command was " + msg);
+		var copybtn = document.getElementById("copybtn");
+		copybtn.value = "copied!";
+		copybtn.innerHTML = "copied!";
+	} catch (err) {
+		console.log("Oops, unable to copy");
+	}
+
+	document.body.removeChild(textArea);
+}
+
+function savem3u() {
+	var link = document.getElementById("link-text").innerText;
+	var source = document.getElementById("sourcelink").innerText;
+	var text = "#EXTM3U \n" + link;
+	var d = Date(Date.now()).split(" ");
+	var datetime = d[3] + "-" + d[2] + "-" + d[4].replaceAll(":", "-");
+	if (source.includes("http")) {
+		s = source.split("/")[2];
+	} else {
+		s = source.split("/")[0];
+	}
+	var blob = new Blob([text], { type: "text/plain;charset=utf-8" });
+	saveAs(blob, filename);
+
+	// console.log(text);
+}
+
+window.addEventListener("load", () => {
+	const preloader = document.querySelector(".preload");
+	preloader.classList.add("preload-finish");
+});
+
+function startAnim() {
+	const preloader = document.querySelector(".preload");
+	preloader.classList.remove("preload-finish");
+}
---
+++ ytde.js
@@ -0,0 +1,85 @@
+// const http = require('http')
+const express = require("express");
+const ytde = require('youtube-dl-exec')
+const fs = require("fs");
+const app = express();
+const port = 8000;
+
+app.use(express.static("public"));
+let m3ulink = "";
+app.set("view-engine", "ejs");
+
+app.get("/search", async (req, res) => {
+	if (!req.query.url) {
+		// res.send("No url provided");
+		res.render("index.ejs", {
+			results: "false",
+			message: 'No link provided',
+			expires: 'none',
+			title: "home",
+		});
+	} else {
+		try {
+			const url = req.query.url;
+			m3ulink = await ytde(url, { getUrl: true });
+			// m3ulink = "https://edge305.stream.highwebmedia.com/live-hls/amlst:ms_seductive-sd-1fde6507b56d2ace0e9fd009ad03e980e9ce21681856bbdc2e0073a4c7c4425e_trns_h264/chunklist_w108457715_b5128000_t64RlBTOjMwLjA=.m3u8"
+			// m3ulink =
+				// "https://manifest.googlevideo.com/api/manifest/hls_playlist/expire/1622143964/ei/fJ-vYNONCc3sW_uNtIgD/ip/105.162.25.171/id/nA9UZF-SZoQ.3/itag/300/source/yt_live_broadcast/requiressl/yes/ratebypass/yes/live/1/sgoap/gir%3Dyes%3Bitag%3D140/sgovp/gir%3Dyes%3Bitag%3D298/hls_chunk_host/r3---sn-n545gpjvh-ocvz.googlevideo.com/playlist_duration/30/manifest_duration/30/vprv/1/playlist_type/DVR/initcwndbps/2970/mh/j6/mm/44/mn/sn-n545gpjvh-ocvz/ms/lva/mv/m/mvi/3/pl/21/dover/11/keepalive/yes/fexp/24001373,24007246/mt/1622122110/sparams/expire,ei,ip,id,itag,source,requiressl,ratebypass,live,sgoap,sgovp,playlist_duration,manifest_duration,vprv,playlist_type/sig/AOq0QJ8wRAIgM1YeWpumdT3Gg8zUyu_bxtw8c8trY_mtsPAVRzCMGBQCIDT2nfSs6_rLqyjZu_X1zEYsc72b1E_j5Fq63bLcYX0o/lsparams/hls_chunk_host,initcwndbps,mh,mm,mn,ms,mv,mvi,pl/lsig/AG3C_xAwRgIhAJ_6z8P35MAf_t2ImJwI-G1j3oQZU4O-Dlg6WtHpRTKIAiEAoeh2MjPg1X513CkjdNwBk5D8TWCEJ7Uk4gxa0E4r_F0%3D/playlist/index.m3u8";
+			message_text = "Here is the link";
+			// output = `<p>${message_text}</p><div id='link'><p>${m3ulink}</p></div><div><button id="copybtn">copy</button></div>`;
+			if (req.query.url.includes("youtube.com")) {
+				var start = m3ulink.indexOf("expire/");
+				var ei = m3ulink.indexOf("/ei");
+				var epoch = Number(m3ulink.substring(start, ei).split("/")[1]);
+				expires = new Date(epoch * 1000);
+			} else {
+				expires = "none";
+			}
+			res.render("index.ejs", {
+				results: "true",
+				message: message_text,
+				m3ulink: m3ulink,
+				sourcelink: req.query.url,
+				expires: expires,
+				title: "home",
+			});
+		} catch (error) {
+			message_text = "Could not get stream link. Please check the url or supported sites";
+			res.render("index.ejs", { results: "false", message: message_text, title: "home", expires: 'none' });
+		}
+	}
+});
+
+app.get("/", (req, res) => {
+	message_text = "";
+	res.render("index.ejs", { results: "false", message: message_text, expires: "none", title: "home" });
+});
+
+app.get("/supported", (req, res) => {
+	const readFileLines = (filename) => fs.readFileSync(filename).toString("UTF8").split("\n");
+	let arr = readFileLines("ytd-list");
+	res.render("supported.ejs", { sites: arr, title: "supported" });
+});
+
+app.get("/donate", (req, res) => {
+	message_text = "";
+	res.render("donate.ejs", { results: "false", message: message_text, expires: "none", title: "donate" });
+});
+
+app.get("/about", (req, res) => {
+	res.render("about.ejs", { title: "about" });
+});
+
+app.get("/faq", (req, res) => {
+	res.render("faq.ejs", { title: "faq" });
+});
+
+app.get("/cutepuppy", (req, res) => {
+	res.render("cutepuppy.ejs", { title: "cutepuppy" });
+});
+
+app.use(function (req, res, next) {
+	res.status(404).render("404.ejs", { title: "404" });
+});
+
+app.listen(port);
