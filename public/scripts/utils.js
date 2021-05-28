3a2ad6fd0797021592120595b5c5cd923cc0db78
keystroke3 <keystroke33@gmail.com>
2021-05-28 22:51:28 +0300

    added name for m3u

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -49,6 +49,7 @@ function savem3u() {
 	} else {
 		s = source.split("/")[0];
 	}
+	filename = s+datetime+'.m3u'
 	var blob = new Blob([text], { type: "text/plain;charset=utf-8" });
 	saveAs(blob, filename);
 
