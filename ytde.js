c5c82d212e32bf1de5aac75409188d2c693bbaa0
keystroke3 <keystroke33@gmail.com>
2021-06-05 15:40:40 +0300

    changed to new logo and background color

--- ytde.js
+++ ytde.js
@@ -7,11 +7,31 @@ const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT;
 
-console.log(port);
+const readQueries = fs.readFileSync("stats.json", "utf8");
+const queries_data = JSON.parse(readQueries);
+let queries = queries_data.queries;
 
 app.use(express.static("public"));
 let m3ulink = "";
 app.set("view-engine", "ejs");
+
+app.get("/", (req, res) => {
+	console.log(queries);
+	message_text = "";
+	res.render("index.ejs", {
+		results: "false",
+		message: message_text,
+		expires: "none",
+		title: "home",
+		queries: queries,
+	});
+});
+
+function countQueries() {
+	queries += 1;
+	queries_data.queries = queries;
+	fs.writeFileSync("stats.json", JSON.stringify(queries_data));
+}
 
 app.get("/search", async (req, res) => {
 	if (!req.query.url) {
@@ -25,14 +45,14 @@ app.get("/search", async (req, res) => {
 	} else {
 		try {
 			const url = req.query.url;
-			m3ulink = await ytde(url, { getUrl: true });
+			// m3ulink = await ytde(url, { getUrl: true });
 			// m3ulink =
-				// "https://edge305.stream.highwebmedia.com/live-hls/amlst:ms_seductive-sd-1fde6507b56d2ace0e9fd009ad03e980e9ce21681856bbdc2e0073a4c7c4425e_trns_h264/chunklist_w108457715_b5128000_t64RlBTOjMwLjA=.m3u8";
-			// m3ulink =
-			// "https://manifest.googlevideo.com/api/manifest/hls_playlist/expire/1622143964/ei/fJ-vYNONCc3sW_uNtIgD/ip/105.162.25.171/id/nA9UZF-SZoQ.3/itag/300/source/yt_live_broadcast/requiressl/yes/ratebypass/yes/live/1/sgoap/gir%3Dyes%3Bitag%3D140/sgovp/gir%3Dyes%3Bitag%3D298/hls_chunk_host/r3---sn-n545gpjvh-ocvz.googlevideo.com/playlist_duration/30/manifest_duration/30/vprv/1/playlist_type/DVR/initcwndbps/2970/mh/j6/mm/44/mn/sn-n545gpjvh-ocvz/ms/lva/mv/m/mvi/3/pl/21/dover/11/keepalive/yes/fexp/24001373,24007246/mt/1622122110/sparams/expire,ei,ip,id,itag,source,requiressl,ratebypass,live,sgoap,sgovp,playlist_duration,manifest_duration,vprv,playlist_type/sig/AOq0QJ8wRAIgM1YeWpumdT3Gg8zUyu_bxtw8c8trY_mtsPAVRzCMGBQCIDT2nfSs6_rLqyjZu_X1zEYsc72b1E_j5Fq63bLcYX0o/lsparams/hls_chunk_host,initcwndbps,mh,mm,mn,ms,mv,mvi,pl/lsig/AG3C_xAwRgIhAJ_6z8P35MAf_t2ImJwI-G1j3oQZU4O-Dlg6WtHpRTKIAiEAoeh2MjPg1X513CkjdNwBk5D8TWCEJ7Uk4gxa0E4r_F0%3D/playlist/index.m3u8";
+			// "https://edge305.stream.highwebmedia.com/live-hls/amlst:ms_seductive-sd-1fde6507b56d2ace0e9fd009ad03e980e9ce21681856bbdc2e0073a4c7c4425e_trns_h264/chunklist_w108457715_b5128000_t64RlBTOjMwLjA=.m3u8";
+			m3ulink =
+				"https://manifest.googlevideo.com/api/manifest/hls_playlist/expire/1622143964/ei/fJ-vYNONCc3sW_uNtIgD/ip/105.162.25.171/id/nA9UZF-SZoQ.3/itag/300/source/yt_live_broadcast/requiressl/yes/ratebypass/yes/live/1/sgoap/gir%3Dyes%3Bitag%3D140/sgovp/gir%3Dyes%3Bitag%3D298/hls_chunk_host/r3---sn-n545gpjvh-ocvz.googlevideo.com/playlist_duration/30/manifest_duration/30/vprv/1/playlist_type/DVR/initcwndbps/2970/mh/j6/mm/44/mn/sn-n545gpjvh-ocvz/ms/lva/mv/m/mvi/3/pl/21/dover/11/keepalive/yes/fexp/24001373,24007246/mt/1622122110/sparams/expire,ei,ip,id,itag,source,requiressl,ratebypass,live,sgoap,sgovp,playlist_duration,manifest_duration,vprv,playlist_type/sig/AOq0QJ8wRAIgM1YeWpumdT3Gg8zUyu_bxtw8c8trY_mtsPAVRzCMGBQCIDT2nfSs6_rLqyjZu_X1zEYsc72b1E_j5Fq63bLcYX0o/lsparams/hls_chunk_host,initcwndbps,mh,mm,mn,ms,mv,mvi,pl/lsig/AG3C_xAwRgIhAJ_6z8P35MAf_t2ImJwI-G1j3oQZU4O-Dlg6WtHpRTKIAiEAoeh2MjPg1X513CkjdNwBk5D8TWCEJ7Uk4gxa0E4r_F0%3D/playlist/index.m3u8";
 			message_text = "Here is the link";
 			// output = `<p>${message_text}</p><div id='link'><p>${m3ulink}</p></div><div><button id="copybtn">copy</button></div>`;
-			if (req.query.url.includes("youtube.com")) {
+			if (req.query.url.includes("youtube")) {
 				var start = m3ulink.indexOf("expire/");
 				var ei = m3ulink.indexOf("/ei");
 				var epoch = Number(m3ulink.substring(start, ei).split("/")[1]);
@@ -48,17 +68,13 @@ app.get("/search", async (req, res) => {
 				sourcelink: req.query.url,
 				expires: expires,
 				title: "home",
+				queries: "none",
 			});
 		} catch (error) {
 			message_text = "Could not get stream link. Please check the url or supported sites";
 			res.render("index.ejs", { results: "false", message: message_text, title: "home", expires: "none" });
 		}
 	}
-});
-
-app.get("/", (req, res) => {
-	message_text = "";
-	res.render("index.ejs", { results: "false", message: message_text, expires: "none", title: "home" });
 });
 
 app.get("/supported", (req, res) => {
@@ -98,7 +114,7 @@ app.get("/headless", async (req, res) => {
 			}
 		} catch (error) {
 			if (req.query.json == "true") {
-				res.json({ error: "unable to locate video link" });
+				res.status(400).json({ error: "unable to locate video link" });
 			} else {
 				res.status(400).send("Unable to locate video link. Incorrect url or service not supported");
 			}
