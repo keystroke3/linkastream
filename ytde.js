160207284a4107dd4cdaf19b5c40dd74dcc35aca
keystroke3 <keystroke33@gmail.com>
2021-06-12 15:26:30 +0300

    added useful error messages and turned on youtube requests

--- ytde.js
+++ ytde.js
@@ -8,6 +8,7 @@ const { reverse } = require("dns");
 dotenv.config();
 const port = process.env.PORT;
 const env = process.env.NODE_ENV;
+// env = 'prod'
 
 // const readQueries = fs.readFileSync("stats.json", "utf8");
 // const queries_data = JSON.parse(readQueries);
@@ -37,20 +38,19 @@ function countQueries() {
 app.get("/search", async (req, res) => {
 	const url = req.query.url;
 	if (!url) {
-		// res.send("No url provided");
 		res.render("index.ejs", {
 			results: "false",
 			message: "No link provided",
 			expires: "none",
 			title: "home",
 		});
-	} else if (url.includes("youtu")) {
-		res.render("index.ejs", {
-			results: "false",
-			message: "Sorry, requests for youtube streams cannot be handled at the moment. Please try again later",
-			expires: "none",
-			title: "home",
-		});
+		// } else if (url.includes("youtu")) {
+		// 	res.render("index.ejs", {
+		// 		results: "false",
+		// 		message: "Sorry, requests for youtube streams cannot be handled at the moment. Please try again later",
+		// 		expires: "none",
+		// 		title: "home",
+		// 	});
 	} else {
 		try {
 			if (env == "prod") {
@@ -58,8 +58,6 @@ app.get("/search", async (req, res) => {
 			} else {
 				m3ulink =
 					"https://edge305.stream.highwebmedia.com/live-hls/amlst:ms_seductive-sd-1fde6507b56d2ace0e9fd009ad03e980e9ce21681856bbdc2e0073a4c7c4425e_trns_h264/chunklist_w108457715_b5128000_t64RlBTOjMwLjA=.m3u8";
-				// m3ulink =
-				// "https://manifest.googlevideo.com/api/manifest/hls_playlist/expire/1622143964/ei/fJ-vYNONCc3sW_uNtIgD/ip/105.162.25.171/id/nA9UZF-SZoQ.3/itag/300/source/yt_live_broadcast/requiressl/yes/ratebypass/yes/live/1/sgoap/gir%3Dyes%3Bitag%3D140/sgovp/gir%3Dyes%3Bitag%3D298/hls_chunk_host/r3---sn-n545gpjvh-ocvz.googlevideo.com/playlist_duration/30/manifest_duration/30/vprv/1/playlist_type/DVR/initcwndbps/2970/mh/j6/mm/44/mn/sn-n545gpjvh-ocvz/ms/lva/mv/m/mvi/3/pl/21/dover/11/keepalive/yes/fexp/24001373,24007246/mt/1622122110/sparams/expire,ei,ip,id,itag,source,requiressl,ratebypass,live,sgoap,sgovp,playlist_duration,manifest_duration,vprv,playlist_type/sig/AOq0QJ8wRAIgM1YeWpumdT3Gg8zUyu_bxtw8c8trY_mtsPAVRzCMGBQCIDT2nfSs6_rLqyjZu_X1zEYsc72b1E_j5Fq63bLcYX0o/lsparams/hls_chunk_host,initcwndbps,mh,mm,mn,ms,mv,mvi,pl/lsig/AG3C_xAwRgIhAJ_6z8P35MAf_t2ImJwI-G1j3oQZU4O-Dlg6WtHpRTKIAiEAoeh2MjPg1X513CkjdNwBk5D8TWCEJ7Uk4gxa0E4r_F0%3D/playlist/index.m3u8";
 			}
 			message_text = "";
 			if (url.includes("youtu")) {
@@ -80,8 +78,18 @@ app.get("/search", async (req, res) => {
 				title: "home",
 				queries: "none",
 			});
-		} catch (error) {
-			message_text = "Could not get stream link. Please check the url or supported sites";
+		} catch (err) {
+			console.log(err)
+			if (err.stderr.includes("too many reqests")) {
+				message_text = "Too many requests. Try again later";
+			} else if (err.stderr.includes("Unsupported") || err.stderr.includes("not known")) {
+				message_text = "Platform unsupported";
+			} else if (err.stderr.includes("unable to extract")) {
+				message_text = "Unable to extract video data";
+			} else {
+				console.log(err);
+				message_text = "Unknown error. Error has been logged";
+			}
 			res.render("index.ejs", { results: "false", message: message_text, title: "home", expires: "none" });
 		}
 	}
@@ -111,18 +119,14 @@ app.get("/cutepuppy", (req, res) => {
 });
 
 app.get("/history", (req, res) => {
-	// if (env == "prod") {
-	// 	res.send("Section under construction. Check back soon.");
-	// } else {
 	res.render("history.ejs", { title: "history" });
-	// }
 });
 app.get("/headless", async (req, res) => {
-	const url = req.query.url
+	const url = req.query.url;
 	if (!url) {
 		res.status(400).send("no url provided");
-	} else if (url.includes("youtu")) {
-		res.status(500).send("Sorry, Youtube stream requests cannot be handled at the moment");
+		// } else if (url.includes("youtu")) {
+		// 	res.status(500).send("Sorry, Youtube stream requests cannot be handled at the moment");
 	} else {
 		try {
 			m3ulink = await ytde(url, { getUrl: true });
